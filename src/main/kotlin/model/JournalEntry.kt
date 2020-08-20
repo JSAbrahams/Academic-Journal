55d015c7113e366e8ae92b9d6659304340e7b667
Joel Abrahams <abrahamsjo@gmail.com>
2020-08-20 15:36:36 +0200

    Add serialization of JournalEntry

--- src/main/kotlin/model/JournalEntry.kt
+++ src/main/kotlin/model/JournalEntry.kt
@@ -2,23 +2,30 @@ package main.kotlin.model
 
 import javafx.beans.property.*
 import javafx.collections.ObservableList
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.descriptors.element
+import kotlinx.serialization.encoding.*
 import tornadofx.ItemViewModel
 import tornadofx.asObservable
 import tornadofx.onChange
 import tornadofx.select
 import java.util.*
 
+@Serializable(with = JournalEntrySerializer::class)
 class JournalEntry(
-    id: UUID? = null,
     creation: Date = Date(),
     lastEdit: Date = Date(),
     title: String = "",
     text: String = "",
-    keywords: List<String> = listOf(),
-    notes: List<Note> = listOf()
+    notes: List<Note> = listOf(),
+    keywords: List<String> = listOf()
 ) {
-    val id = id ?: UUID.randomUUID()
-
     val lastEditProperty = SimpleObjectProperty(lastEdit)
     val creationProperty = SimpleObjectProperty(creation)
 
@@ -27,8 +34,8 @@ class JournalEntry(
     val titleProperty = SimpleStringProperty(title)
     val textProperty = SimpleStringProperty(text)
 
-    val notesProperty = SimpleListProperty<Note>(notes.asObservable())
-    val keywordsProperty = SimpleListProperty<String>(keywords.asObservable())
+    val notesProperty = SimpleListProperty(notes.asObservable())
+    val keywordsProperty = SimpleListProperty(keywords.asObservable())
 
     init {
         titleProperty.onChange { editedProperty.set(true) }
@@ -37,8 +44,72 @@ class JournalEntry(
         keywordsProperty.onChange<ObservableList<String>> { editedProperty.set(true) }
     }
 
-    override fun equals(other: Any?): Boolean = other is JournalEntry && id == other.id
-    override fun hashCode(): Int = id.hashCode()
+    override fun equals(other: Any?): Boolean = other is JournalEntry
+            && lastEditProperty.get().toInstant().epochSecond == other.lastEditProperty.get().toInstant().epochSecond
+            && creationProperty.get().toInstant().epochSecond == other.creationProperty.get().toInstant().epochSecond
+            && editedProperty.get() == other.editedProperty.get()
+            && titleProperty.get() == other.titleProperty.get()
+            && textProperty.get() == other.textProperty.get()
+            && notesProperty.get().toList() == other.notesProperty.get().toList()
+            && keywordsProperty.get().toList() == other.keywordsProperty.get().toList()
+
+    override fun hashCode(): Int {
+        var result = lastEditProperty.get().toInstant().epochSecond.hashCode()
+        result = 31 * result + creationProperty.get().toInstant().epochSecond.hashCode()
+        result = 31 * result + editedProperty.hashCode()
+        result = 31 * result + titleProperty.hashCode()
+        result = 31 * result + textProperty.hashCode()
+        result = 31 * result + notesProperty.hashCode()
+        result = 31 * result + keywordsProperty.hashCode()
+        return result
+    }
+}
+
+
+/**
+ * Custom JournalEntrySerializer to handle the ObjectProperty's.
+ */
+object JournalEntrySerializer : KSerializer<JournalEntry> {
+    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("JournalEntry") {
+        element<Long>("creation")
+        element<Long>("last_edited")
+        element<String>("title")
+        element<String>("text")
+        element<List<Note>>("notes")
+        element<List<String>>("keywords")
+    }
+
+    override fun serialize(encoder: Encoder, value: JournalEntry) = encoder.encodeStructure(descriptor) {
+        encodeLongElement(descriptor, 0, value.creationProperty.get().toInstant().epochSecond)
+        encodeLongElement(descriptor, 1, value.lastEditProperty.get().toInstant().epochSecond)
+        encodeStringElement(descriptor, 2, value.titleProperty.get())
+        encodeStringElement(descriptor, 3, value.textProperty.get())
+        encodeSerializableElement(descriptor, 4, ListSerializer(NoteSerializer), value.notesProperty.toList())
+        encodeSerializableElement(descriptor, 5, ListSerializer(String.serializer()), value.keywordsProperty.toList())
+    }
+
+    override fun deserialize(decoder: Decoder): JournalEntry = decoder.decodeStructure(descriptor) {
+        var lastEdit = Date()
+        var creation = Date()
+        var title = ""
+        var text = ""
+        val notes = mutableListOf<Note>()
+        val keywords = mutableListOf<String>()
+        while (true) {
+            when (val index = decodeElementIndex(descriptor)) {
+                0 -> creation = Date(decodeLongElement(descriptor, index) * 1000)
+                1 -> lastEdit = Date(decodeLongElement(descriptor, index) * 1000)
+                2 -> title = decodeStringElement(descriptor, index)
+                3 -> text = decodeStringElement(descriptor, index)
+                4 -> notes.addAll(decodeSerializableElement(descriptor, index, ListSerializer(NoteSerializer)))
+                5 -> keywords.addAll(decodeSerializableElement(descriptor, index, ListSerializer(String.serializer())))
+                CompositeDecoder.DECODE_DONE -> break
+                else -> throw SerializationException("Unknown index $index")
+            }
+        }
+
+        JournalEntry(creation, lastEdit, title, text, notes, keywords)
+    }
 }
 
 class JournalEntryModel(property: ObjectProperty<JournalEntry>) :
---
+++ src/test/kotlin/model/JournalEntryTest.kt
@@ -0,0 +1,30 @@
+package model
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import main.kotlin.model.JournalEntry
+import java.util.*
+
+class JournalEntryTest : FreeSpec({
+    "an journal entry" - {
+        "can be retrieved from a json" {
+            val string = this.javaClass.getResource("/json/journal_entry/simple.json").readText()
+            val journalEntry = Json.decodeFromString<JournalEntry>(string)
+
+            journalEntry.titleProperty.get() shouldBe "testTitle"
+            journalEntry.textProperty.get() shouldBe "text"
+            journalEntry.creationProperty.get().toInstant().epochSecond shouldBe 10
+            journalEntry.lastEditProperty.get().toInstant().epochSecond shouldBe 20
+        }
+        "can be written to json" {
+            val entry = JournalEntry(Date(100), Date(200), "title", "text")
+            val entryString = Json.encodeToString(entry)
+            val newEntry = Json.decodeFromString<JournalEntry>(entryString)
+
+            newEntry shouldBe entry
+        }
+    }
+})
