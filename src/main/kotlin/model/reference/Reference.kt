0f32972d29b312e59a822bc3a249a93dc43ed2b0
Joel Abrahams <abrahamsjo@gmail.com>
2020-08-28 22:07:33 +0200

    Add properties for all reference values

--- src/main/kotlin/model/reference/Reference.kt
+++ src/main/kotlin/model/reference/Reference.kt
@@ -1,38 +1,50 @@
 package main.kotlin.model.reference
 
-import javafx.beans.property.ObjectProperty
-import javafx.beans.property.SimpleListProperty
-import javafx.beans.property.SimpleStringProperty
+import javafx.beans.property.*
 import javafx.collections.ObservableList
 import tornadofx.ItemViewModel
 import tornadofx.asObservable
 import tornadofx.select
 import java.time.LocalDate
 
-data class Reference(
+class Reference(
     val id: Int = -1,
-    val itemType: String = "",
-    val title: String = "",
-    val authors: List<Author> = listOf(),
-    val abstract: String = "",
+    itemType: String = "",
+    title: String = "",
+    authors: List<Author> = listOf(),
+    abstract: String = "",
 
-    val publication: Int? = null,
-    val volume: Int? = null,
-    val issue: Int? = null,
-    val pages: Pair<Int?, Int?> = Pair(null, null),
+    publication: String = "",
+    volume: String = "",
+    issue: String = "",
+    pages: Pair<Int, Int> = Pair(-1, -1),
 
-    val date: LocalDate? = null,
-    val series: String = "",
-    val seriesTitle: String = "",
-    val seriesText: String = "",
-
-    val journalAbbr: String = "",
-    val language: String = "",
+    date: LocalDate = LocalDate.MIN,
+    series: String = "",
+    seriesTitle: String = "",
+    seriesText: String = "",
+    journalAbbr: String = "",
+    language: String = "",
 ) {
     val itemTypeProperty = SimpleStringProperty(itemType)
     val titleProperty = SimpleStringProperty(title)
     val authorProperty = SimpleListProperty(authors.asObservable())
     val abstractProperty = SimpleStringProperty(abstract)
+
+    val publicationProperty = SimpleStringProperty(publication)
+    val volumeProperty = SimpleStringProperty(volume)
+    val issueProperty = SimpleStringProperty(issue)
+
+    val pagesStartProperty = SimpleIntegerProperty(pages.first)
+    val pagesEndProperty = SimpleIntegerProperty(pages.second)
+
+    val dateProperty = SimpleObjectProperty(date)
+    val seriesProperty = SimpleStringProperty(series)
+    val seriesTitleProperty = SimpleStringProperty(seriesTitle)
+    val seriesTextProperty = SimpleStringProperty(seriesText)
+    val journalAbbrProperty = SimpleStringProperty(journalAbbr)
+
+    val languageProperty = SimpleStringProperty(language)
 }
 
 class ReferenceModel(property: ObjectProperty<Reference>) : ItemViewModel<Reference>(itemProperty = property) {
@@ -40,5 +52,20 @@ class ReferenceModel(property: ObjectProperty<Reference>) : ItemViewModel<Refere
     val title = bind(autocommit = true) { property.select { it.titleProperty } }
     val authors: ObservableList<Author> = bind(autocommit = true) { property.select { it.authorProperty } }
     val abstract = bind(autocommit = true) { property.select { it.abstractProperty } }
+
+    val publication = bind(autocommit = true) { property.select { it.publicationProperty } }
+    val volume = bind(autocommit = true) { property.select { it.volumeProperty } }
+    val issue = bind(autocommit = true) { property.select { it.issueProperty } }
+
+    val pagesStart = bind(autocommit = true) { property.select { it.pagesStartProperty } }
+    val pagesEnd = bind(autocommit = true) { property.select { it.pagesEndProperty } }
+
+    val date = bind(autocommit = true) { property.select { it.dateProperty } }
+    val series = bind(autocommit = true) { property.select { it.seriesProperty } }
+    val seriesTitle = bind(autocommit = true) { property.select { it.seriesTitleProperty } }
+    val seriesText = bind(autocommit = true) { property.select { it.seriesTextProperty } }
+    val journalAbbr = bind(autocommit = true) { property.select { it.journalAbbrProperty } }
+
+    val language = bind(autocommit = true) { property.select { it.languageProperty } }
 }
 
